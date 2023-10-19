89932bc7c4467437cc963ff54643798c8d8248d3
ibrahivan <ivan.vazquez.cod@gmail.com>
2023-10-19 20:01:03 +0200

    Initial commit

---
+++ Scaffolding/Empleado.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scaffolding;
+
+public partial class Empleado
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+}
